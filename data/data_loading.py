817e0f7b35ec435c491580ae64af1ff4b2cd1329
Tom Holmes <s2070797@ed.ac.uk>
2021-05-25 13:07:20 +0100

    comments

--- data/data_loading.py
+++ data/data_loading.py
@@ -2,11 +2,30 @@ import numpy as np
 import pandas as pd
 
 def to_csv(data, filename) -> None:
+    """
+    Converts a dataset from np.array to pd.DataFrame then writes to a .csv file
+
+    Input:
+        data (np.array): data to be written to csv
+        filename (str): filename/path of csv to be written to
+    
+    Return:
+        None
+    """
     df = pd.DataFrame(data)
     df.to_csv(r"{}".format(filename))
     return None
 
 def from_csv(filename) -> np.array:
+    """
+    Reads a dataset in .csv format and converts to a np.array
+
+    Input:
+        filename (str): filename/path of csv to be read
+    
+    Return:
+        out (np.array): data in np.array format
+    """
     df = pd.read_csv(r"{}".format(filename))
     out = df.to_numpy()
     return out
