74bac93fde894b69093f3585fb1434675babf8c7
Tom Holmes <s2070797@ed.ac.uk>
2021-06-05 12:05:39 +0100

    testing

--- src/test2.py
+++ src/test2.py
@@ -8,7 +8,7 @@ test_data = data.generation.normal(0,1,1000,2)
 print("finished data")
 
 start_time = timeit.default_timer()
-ball = MEB(radius=5).fit(test_data, eps=1e-4, method="exact")
+ball = MEB(radius=5).fit(test_data, eps=1e-4, method="socp_heuristic")
 elapsed = timeit.default_timer() - start_time
 print("Elapsed: {}".format(elapsed))
 
