6b483f439aaa0ab74fdaff6e581145f5d7f73512
Tom Holmes <s2070797@ed.ac.uk>
2021-06-06 16:52:44 +0100

    testing

--- src/test2.py
+++ src/test2.py
@@ -4,11 +4,11 @@ import numpy as np
 
 import timeit
 
-test_data = data.generation.normal(0,1,1000,50)
+test_data = data.generation.normal(0,1,10000,2)
 
 
 start_time = timeit.default_timer()
-ball = MEB().fit(test_data, eps=1e-4, method="socp_heuristic")
+ball = MEB().fit(test_data, eps=1e-2, method="heuristic_1")
 print(ball.check_subset(test_data))
 elapsed = timeit.default_timer() - start_time
 print("Elapsed: {}".format(elapsed))
