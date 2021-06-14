a23255bc10efeb35bbde29f40d807e164dcaebef
Tom Holmes <s2070797@ed.ac.uk>
2021-06-14 18:33:28 +0100

    get exact MEBwO working

--- src/test/gurobi_test.py
+++ src/test/gurobi_test.py
@@ -4,7 +4,7 @@ import gurobipy as gp
 from gurobipy import GRB
 
 d = 2 # dimension
-n = 10 # number of data points
+n = 1000 # number of data points
 eta = 0.9
 k = n*(1-eta)
 M = 10
@@ -21,52 +21,45 @@ data = gen_data(n,d)
 
 m = gp.Model("MEB")
 
-#m.Params.NonConvex = 2
+# m.Params.NonConvex = 2
 
-c = m.addMVar(shape=d, lb=-GRB.INFINITY, name="center")
-xi = m.addMVar(shape=n, vtype=GRB.BINARY)
+c = m.addVars(d, lb=-GRB.INFINITY, name="center")
+xi = m.addVars(n, vtype=GRB.BINARY)
 r = m.addVar(name="radius")
+
+y = m.addVars(d)
 
 m.setObjective(r, GRB.MINIMIZE)
 
-Q = np.identity(d)
-print(Q)
 for i in range(n):
-    m.addMQConstr(
-        Q=Q,
-        c=np.concatenate((-2*data[i], -1*(M**2)*np.ones(n), -1), axis=None),
-        sense=GRB.LESS_EQUAL,
-        rhs= -1*(data[i]@data[i]),
-        xQ_L=c.tolist(),
-        xQ_R=c.tolist(),
-        xc=c.tolist() + xi.tolist() + [r]
+    m.addConstr(
+        sum([c[j]*c[j] - 2*c[j]*data[i][j] + (data[i][j]**2) for j in range(d)]) - r - M*xi[i] <= 0
     )
 
 m.addConstr(sum(xi[i] for i in range(n)) <= k)
 
 m.optimize()
 
-center = [v.x for v in c.tolist()]
+center = [c[i].x for i in range(d)]
 radius = np.sqrt(m.getVarByName(name="radius").x)
 
 print("center:\t", center)
 print("radius:\t", radius)
-print("xi:\t", [v.x for v in xi.tolist()])
+print("xi:\t", [xi[i].x for i in range(n)])
 
-fig,ax = plt.subplots(figsize=(8,8))
-
-
+if d == 2:
+    fig,ax = plt.subplots(figsize=(8,8))
             
-x = [data[i][0] for i in range(n)]
-y = [data[i][1] for i in range(n)]
+    x = [data[i][0] for i in range(n)]
+    y = [data[i][1] for i in range(n)]
 
-plt.scatter(x, y, color="blue", label="data")    
+    plt.scatter(x, y, color="blue", label="data")    
 
-plt.scatter(center[0], center[1], color="red", marker="x", label="center")
-ax.add_patch(
-    plt.Circle(center, radius, color="red", fill=False, label="ball")
-)
+    plt.scatter(center[0], center[1], color="red", marker="x", label="center")
+    ax.add_patch(
+        plt.Circle(center, radius, color="red", fill=False, label="ball")
+    )
 
-plt.legend()
+    plt.legend()
 
-plt.show()
+    plt.show()
