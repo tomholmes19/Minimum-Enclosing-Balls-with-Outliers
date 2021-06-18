feab827a087214d4703c0ec118bc42b0a8c3c9fa
Tom Holmes <s2070797@ed.ac.uk>
2021-06-18 16:29:56 +0100

    create uniform ball generation function

--- src/data/generation.py
+++ src/data/generation.py
@@ -11,7 +11,7 @@ def normal(mean, variance, n, dimension) -> np.array:
         n (int): number of data points to generate
         dimension (int): dimension of the data points
 
-    Out:
+    Return:
         data (np.array): generated data 
     """
     data = np.array([np.random.normal(mean, variance, dimension) for _ in range(n)])
@@ -26,7 +26,7 @@ def uniform(lb, ub, n, dimension) -> np.array:
         n (int): number of data points to generate
         dimension (int): dimension of the data points
 
-    Out:
+    Return:
         data (np.array): generated data 
     """
     data = np.array([np.random.uniform(lb, ub, dimension) for _ in range(n)])
@@ -40,7 +40,7 @@ def poisson(lam, n, dimension) -> np.array:
         n (int): number of data points to generate
         dimension (int): dimension of the data points
 
-    Out:
+    Return:
         data (np.array): generated data 
     """
     data = np.array([np.random.poisson(lam, dimension) for _ in range(n)])
@@ -49,3 +49,47 @@ def poisson(lam, n, dimension) -> np.array:
 def cube():
     #TODO: find out what this means
     pass
+
+def uniform_point_in_ball(d, lb, ub):
+    """
+    Generates a uniform point in d-dimensions in a ball with minimum length lb and maximum length ub
+
+    Input:
+        d (int): dimension of points
+        lb (float): lower bound for length
+        ub (float): ubber bound for length
+    
+    Return:
+        point (np.array): data point
+    """
+    length = np.random.uniform(low=lb, high=ub)
+    direction = np.random.uniform(low=-1, high=1, size=d)
+    direction = direction/np.linalg.norm(direction)
+    point = length*direction
+    return point
+
+def uniform_ball_with_ouliters(n, d, eta, c, r1, r2) -> np.array:
+    """
+    Generates n many data points where eta*n many are uniformly inside a ball B1 with centre c and radius r1,
+    and (1-eta)*n many are uniformly inside a ball B2 with centre c and radius r2 but not in B1
+
+    Input:
+        n (int): total number of data points to generate
+        d (int): dimension of data points
+        eta (float): percentage of data points to be inside B1
+        c (np.array): centre of balls
+        r1 (float): radius of inner ball
+        r2 (float): radius of outer blal
+    
+    Return:
+        data (np.array): generated data
+    """
+    n_inner = int(np.floor(eta*n))
+    n_outer = int(np.ceil((1-eta)*n))
+
+    data_inner = [uniform_point_in_ball(d=d, lb=0, ub=r1) for _ in range(n_inner)]
+    data_outer = [uniform_point_in_ball(d=d, lb=r1, ub=r2) for _ in range(n_outer)]
+
+    data = data_inner + data_outer
+    np.random.shuffle(data)
+    return np.array(data)
--- src/test2.py
+++ src/test2.py
@@ -4,10 +4,11 @@ import numpy as np
 
 import timeit
 
-test_data = data.generation.normal(0,1,1000,2)
+test_data = data.generation.uniform_ball_with_ouliters(n=10000, d=2, eta=0.9, c=np.array([0,0]), r1=1, r2=5)
 start = timeit.default_timer()
-ball = MEBwO().fit(data=test_data, method="exact", eta=0.9)
+#ball = MEBwO().fit(data=test_data, method="exact", eta=0.9)
+ball = Ball(center=np.array([0,0]), radius=1)
 elapsed = timeit.default_timer() - start
 print("Total time:\t", elapsed)
-print("Contains:\t{}%".format(ball.pct_containment(test_data)*100))
+#print("Contains:\t{}%".format(ball.pct_containment(test_data)*100))
 ball.plot(data=test_data)
