2165f127ebef75d14e0e6411a8ed155e20a4de2c
Tom Holmes <s2070797@ed.ac.uk>
2021-06-21 17:01:19 +0100

    add mnist dataset

---
+++ src/mnist_data.py
@@ -0,0 +1,17 @@
+import numpy as np
+import tensorflow_datasets as tfds
+
+from data.loading import to_csv
+
+ds = tfds.load("mnist", split="train", as_supervised=True)
+
+data = []
+
+for image, label in tfds.as_numpy(ds):
+    im = np.array([image[i].flatten() for i in range(28)]).flatten()
+    im = np.insert(im, 0, label)
+    data.append(im)
+
+data = np.array(data)
+
+to_csv(data, filename="src\datasets\mnist\mnist_train.csv")
