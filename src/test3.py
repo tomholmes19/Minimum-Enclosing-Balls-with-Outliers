d768ece5b619c3aafd7c8908d3c0effebb83ee24
Thomas Holmes <holmesthomas19@gmail.com>
2021-07-10 16:57:54 +0100

    test get_times_from_log

--- src/test3.py
+++ src/test3.py
@@ -1,25 +1,16 @@
-import timeit
+import benchmarking.utils
 
-from data.generation import normal
-from meb.geometry import M_estimate
-from meb.ball import Ball
-from meb.mebwo_algorithms import M_approx
+n = [50+ 50*i for i in range(10)]
+eta = 0.9
+d = 8
+times = benchmarking.utils.get_times_from_log(filepath=r"src/test/test_log.log")
 
-data = normal(0, 1, 1000, 2)
-print("Calculating M1")
-M1_start = timeit.default_timer()
-M1 = M_estimate(data)
-M1_elapsed = timeit.default_timer() - M1_start
-
-print("Calculating M2")
-M2_start = timeit.default_timer()
-M2 = M_approx(data)
-M2_elapsed = timeit.default_timer() - M2_start
-
-print("M1:\t\t{}".format(M1))
-print("Elapsed:\t{}s".format(M1_elapsed))
-
-print("M2:\t\t{}".format(M2))
-print("Elapsed:\t{}s".format(M2_elapsed))
-
-Ball(radius=1, center=[0,0]).plot(data)
+benchmarking.utils.plot_times(
+        x_axis=n,
+        times=times,
+        xlabel="n",
+        ylabel="Time",
+        title="Running time for MEBwO as a function of n, d={}, eta={}".format(d, eta),
+        plot=True,
+        filepath=r"images/benchmarks/mebwo_runtimes_d{}.png".format(d)
+    )
