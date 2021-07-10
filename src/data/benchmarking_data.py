7bf07a44dda0f9e9002f2c87162b24b95f134594
Thomas Holmes <holmesthomas19@gmail.com>
2021-07-10 17:55:12 +0100

    refactor

--- src/data/benchmarking_data.py
+++ src/data/benchmarking_data.py
@@ -1,16 +1,56 @@
 import numpy as np
+from pathlib import Path
 
-from loading import to_csv
+import loading
 import generation
+
+def wrapper(data_type, func, **kwargs) -> None:
+    """
+    Generates data using func, saves to filepath, prints progress
+    """
+    filename = r"datasets/{}.csv".format(data_type)
+    run = True
+
+    if Path(filename).exists():
+        response = input("File {} already exists. Continue? (y/n)\n".format(filename))
+        if response == "n":
+            run = False
+
+    if run:
+        print("Generating {}".format(data_type))
+        data = func(**kwargs)
+        print("Saving {}".format(data_type))
+        loading.to_csv(data=data, filename=r"datasets/{}.csv".format(data_type))
+
+    return None
 
 n = 100000
 d = 1000
 
-np.random.seed(seed=10)
+np.random.seed(seed=1234)
 
-print("Generating data")
-normal_data = generation.normal(mean=0, variance=1, n=n, dimension=d)
-print("Finished generating")
-print("Saving data")
-to_csv(data=normal_data, filename=r"datasets/normal.csv")
-print("Finished saving")
+wrapper(
+    "normal",
+    generation.normal,
+    mean=0,
+    variance=1,
+    n=n,
+    dimension=d
+)
+
+wrapper(
+    "uniform",
+    generation.uniform,
+    lb=0,
+    ub=1,
+    n=n,
+    dimension=d
+)
+
+wrapper(
+    "poisson",
+    generation.poisson,
+    lam=1,
+    n=n,
+    dimension=d
+)
