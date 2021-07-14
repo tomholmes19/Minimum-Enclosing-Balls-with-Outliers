1a5204967720b20ba4ff737ac33973edd70e8d55
Thomas Holmes <holmesthomas19@gmail.com>
2021-07-14 22:18:45 +0100

    create Q function

--- src/test.py
+++ src/test.py
@@ -1,3 +1,30 @@
-import subprocess
+import numpy as np
 
-subprocess.call(["sh", "src/notify.sh"])
+from meb import geometry
+
+from meb.ball import MEBwO
+from data import generation
+
+np.random.seed(1234)
+data = generation.normal(0,10,1000,100)
+
+ball = MEBwO().fit(data=data, method="shenmaier", eta=0.9)
+print("Finished Shenmaier")
+ball.plot(data=data, show=False)
+c = ball.center
+r = ball.radius
+gamma = r**2
+
+inliers = [x for x in data if ball.contains(x)]
+a = geometry.find_furthest(c, inliers)
+
+x = min([geometry.Q(c, a, point, gamma) for point in inliers])
+
+new_c = c - (c-a)*x/2
+new_r = max([np.linalg.norm(new_c - point) for point in inliers])
+
+new_ball = MEBwO(new_c, new_r)
+
+print(r, new_r)
+
+new_ball.plot(data=data)
