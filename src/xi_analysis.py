2b4fa3c77788ffdd3b40abb14a63d37bcc4d66e0
Thomas Holmes <holmesthomas19@gmail.com>
2021-07-15 18:37:56 +0100

    rename function

--- src/xi_analysis.py
+++ src/xi_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from meb.gurobi_solvers import mebwo_exact
+from meb.gurobi_solvers import mebwo
 from data.loading import from_csv, subset_data
 from meb.geometry import M_estimate
 
@@ -17,7 +17,7 @@ M_list = [M_UB*k/2 for k in range(1,num_trials+1)]
 variances = {M: None for M in M_list}
 
 for M in M_list:
-    _, _, xi, _ = mebwo_exact(data=data, eta=eta, M=M, relax=True)
+    _, _, xi, _ = mebwo(data=data, eta=eta, M=M, relax=True)
     variances[M] = np.var(xi)
 
 plt.plot(variances.keys(), variances.values(), marker="o", linestyle=":", mec="r", mfc="r")
