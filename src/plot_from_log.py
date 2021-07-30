a55140dc3d0037ae336777ec4f4cbd1cb3926e3a
Thomas Holmes <holmesthomas19@gmail.com>
2021-07-30 12:52:51 +0100

    benchmarking

--- src/plot_from_log.py
+++ src/plot_from_log.py
@@ -2,20 +2,28 @@ import benchmarking.utils
 import data.loading
 from meb import geometry
 
-n = 10000
-d = [10+10*i for i in range(10)]
+n = [1000+3000*i for i in range(10) if 1000+3000*i < 25000]
+d = 30
 eta = 0.9
 
-filep = r"benchmarks/shenmaier/uniform_ball/func_d_n10000_eta0p9_uniform_ball"
+func_names = {
+        "relaxation_heuristic": "Relaxation-Based Heuristic",
+        "shrink": "Shrink Heuristic",
+        "shrink_avg": "Shrink (avg) Heuristic",
+        "shenmaier": "Shenmaier's Approximation"
+    }
+
+filep = r"benchmarks/relaxation_heuristic/uniform_ball_with_outliers/func_n_d30_eta0p9_uniform_ball_with_outliers"
+func_name = filep.split("/")[1]
 
 times = benchmarking.utils.get_times_from_log(filepath=r"{}.log".format(filep))
-
+title = benchmarking.utils.get_title(func_name, n, d, eta)
 benchmarking.utils.plot_times(
-        x_axis=d,
+        x_axis=n,
         times=times,
         xlabel="n",
         ylabel="Time",
-        title="Runtime for Shenmaier's Approximation as a function of d, n={0}, eta={1}".format(n,eta),
+        title=title,
         plot=True,
         filepath=r"{}.png".format(filep)
     )
