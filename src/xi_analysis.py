5401628f2c20ed5ed9759a8871436efe2bd4b3d5
Thomas Holmes <holmesthomas19@gmail.com>
2021-07-31 13:52:02 +0100

    plots

--- src/xi_analysis.py
+++ src/xi_analysis.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import plot_settings
 
+import data.generation
 from meb.gurobi_solvers import mebwo
-from data.loading import from_csv, subset_data
 from meb.geometry import M_estimate
 
-data = from_csv(r"datasets/normal.csv")
-data = subset_data(data, rows=range(300), columns=range(10))
+np.random.seed(1234)
+data = data.generation.normal(0,1,300,10)
 
 eta = 0.9
 M_UB = M_estimate(data)
@@ -17,11 +19,12 @@ M_list = [M_UB*k/2 for k in range(1,num_trials+1)]
 variances = {M: None for M in M_list}
 
 for M in M_list:
-    _, _, xi, _ = mebwo(data=data, eta=eta, M=M, relax=True)
+    _, _, xi, _ = mebwo(data=data, eta=eta, M=M, relax=True, outputflag=1)
     variances[M] = np.var(xi)
 
-plt.plot(variances.keys(), variances.values(), marker="o", linestyle=":", mec="r", mfc="r")
-
+# plt.plot(variances.keys(), variances.values(), marker="o", linestyle=":", mec="b", mfc="b")
+sns.lineplot(x=M_list, y=variances.values())
 plt.xlabel("M")
 plt.ylabel("Variance")
+plt.savefig(r"images/xi_analysis.png")
 plt.show()
