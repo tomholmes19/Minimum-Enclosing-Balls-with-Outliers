38fb46cf2a9e5a0a777b6ae9bcd3ca4b6e2fcf22
Thomas Holmes <holmesthomas19@gmail.com>
2021-08-02 18:03:04 +0100

    set plot style

--- src/core_set_example.py
+++ src/core_set_example.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from meb.ball import MEB
 import data.generation
+import plot_settings
 
 n = 1000
 d = 2
@@ -11,4 +13,8 @@ np.random.seed(1000)
 data = data.generation.normal(0, 1, n, d)
 
 ball = MEB().fit(data, method="socp_heuristic", eps=1e-4)
-ball.plot(data)
+
+ball.plot(data, show=False, figsize=8.5)
+plt.legend()
+plt.savefig(r"images/core_set_example.png", bbox_inches='tight')
+plt.show()
