abfa7ff195c89d53a9ec87867833ece270c65768
Thomas Holmes <holmesthomas19@gmail.com>
2021-08-08 09:46:28 +0100

    benchmarking

--- src/plot_from_log.py
+++ src/plot_from_log.py
@@ -3,11 +3,11 @@ import data.loading
 from meb import geometry
 
 
-n = [1000 + 3000*i for i in range(10)]
-d = 30
+#n = [1000 + 3000*i for i in range(10)]
+#d = 30
 
-#n = 10000
-#d = [10+10*i for i in range(10)]
+n = 10000
+d = [10+10*i for i in range(8)]
 
 
 eta = 0.9
@@ -19,13 +19,13 @@ func_names = {
         "shenmaier": "Shenmaier's Approximation"
     }
 
-filep = r"benchmarks/relaxation_heuristic/normal/func_n_d30_eta0p9_normal"
+filep = r"benchmarks/relaxation_heuristic/uniform_ball_with_outliers/func_d_n10000_eta0p9_uniform_ball_with_outliers"
 func_name = filep.split("/")[1]
 
 times = benchmarking.utils.get_times_from_log(filepath=r"{}.log".format(filep))
 title = benchmarking.utils.get_title(func_name, n, d, eta)
 benchmarking.utils.plot_times(
-        x_axis=n,
+        x_axis=d,
         times=times,
         xlabel="n",
         ylabel="Time",
