e8e5ca993e77aee9e62bb8f2ddc765fc49d49b92
Thomas Holmes <holmesthomas19@gmail.com>
2021-08-14 17:50:56 +0100

    plots

--- src/alg_plots.py
+++ src/alg_plots.py
@@ -9,25 +9,45 @@ import benchmarking.utils
 
 heuristics = ["relaxation_heuristic", "shenmaier", "shrink"]
 data_types = ["hyperspherical_shell", "normal", "uniform_ball", "uniform_ball_with_outliers"]
-#data_types = ["hyperspherical_shell", "uniform_ball"]
 params = ["n", "d", "eta"]
 
-colours = {
+colours_funcs = {
     "relaxation_heuristic": "r",
     "shenmaier": "b",
     "shrink": "g"
 }
 
-markers = {
+colours_data = {
+    "hyperspherical_shell": "r",
+    "normal": "b",
+    "uniform_ball": "g",
+    "uniform_ball_with_outliers": "m"
+}
+
+markers_funcs = {
     "relaxation_heuristic": "o",
     "shenmaier": "^",
     "shrink": "D"
 }
 
-labels = {
+markers_data = {
+    "hyperspherical_shell": "o",
+    "normal": "^",
+    "uniform_ball": "D",
+    "uniform_ball_with_outliers": "s"
+}
+
+labels_funcs = {
     "relaxation_heuristic": "RBH",
     "shenmaier": "Shenmaier",
     "shrink": "Shrink"
+}
+
+labels_data = {
+    "hyperspherical_shell": "Hyp. Shell",
+    "normal": "Normal",
+    "uniform_ball": "Uniform Ball",
+    "uniform_ball_with_outliers": "UBwO"
 }
 
 axes = {
@@ -65,11 +85,45 @@ for data_type in data_types:
             else:
                 x_axis = axes[param]
 
-            plt.plot(x_axis, times, marker=markers[heuristic], color=colours[heuristic], linestyle=":", label=labels[heuristic])
+            plt.plot(x_axis, times, marker=markers_funcs[heuristic], color=colours_funcs[heuristic], linestyle=":", label=labels_funcs[heuristic])
         
         if data_type == "normal" and param == "eta":
             plt.legend(loc="best", bbox_to_anchor=(0.5,0.,0.5,0.5))
         else:
             plt.legend()
 
-        plt.savefig("images/alg_benchmarks/{}".format(filename), bbox_inches="tight")
+        plt.savefig("images/alg_benchmarks/by_data/{}".format(filename), bbox_inches="tight")
+        plt.close()
+
+for heuristic in heuristics:
+    for param in params:
+        # construct filename
+        filename = "func_{0}_{1}".format(param, fixed_params[param])
+        if param != "eta":
+            filename += "_eta0p9"
+
+        # get times from log
+        times_dict = {data_type: benchmarking.utils.get_times_from_log(r"benchmarks/{0}/{1}/{2}_{3}.log".format(heuristic, data_type, filename, data_type)) for data_type in data_types}
+        
+        plt.figure()
+        plt.xlabel(param)
+        plt.ylabel("Time (s)")
+
+        for data_type, times in times_dict.items():
+            if heuristic == "shenmaier" and param == "n":
+                x_axis = [1000 + 3000*i for i in range(8)]
+            elif heuristic == "relaxation_heuristic" and data_type == "uniform_ball_with_outliers" and param == "d":
+                x_axis = [10 + 10*i for i in range(8)]
+            else:
+                x_axis = axes[param]
+
+            
+            plt.plot(x_axis, times, marker=markers_data[data_type], color=colours_data[data_type], linestyle=":", label=labels_data[data_type])
+        
+        if heuristic == "relaxation_heuristic" and param == "eta":
+            plt.legend(loc="best", bbox_to_anchor=(0.45,0.45))
+        else:
+            plt.legend()
+        
+        plt.savefig(r"images/alg_benchmarks/by_func/{0}_{1}".format(heuristic, filename), bbox_inches="tight")
+        plt.close()
