e83fc94284666859b3184e584861a247a250052a
Thomas Holmes <holmesthomas19@gmail.com>
2021-08-17 13:17:32 +0100

    tables

--- src/alg_tables.py
+++ src/alg_tables.py
@@ -16,16 +16,23 @@ fixed_params = {
     "eta": "n10000_d30"
 }
 
-column_headers = [
-    "RBH",
-    "Shenmaier",
-    "Shrink Heuristic",
-    "Avg. Point Heuristic"
-]
+param_latex = {
+    "n": "$n$",
+    "d": "$d$",
+    "eta": "$\\eta$"
+}
+
+column_headers = {
+    "relaxation_heuristic": "RBH",
+    "shenmaier": "Shenm.",
+    "shrink": "Shrink",
+    "shrink_avg": "APH"
+}
 
 for data_type in data_types:
     for param in params:
-        results_dict = {param: params[param], **{heuristic: None for heuristic in heuristics}}
+        results_dict = {heuristic: None for heuristic in heuristics}
+        times_dict = {heuristic: None for heuristic in heuristics}
         
         for heuristic in heuristics:
             # construct filename
@@ -34,14 +41,24 @@ for data_type in data_types:
                 filename += "_eta0p9"
             filename += "_{}".format(data_type)
             
-            results = benchmarking.utils.get_r_from_log(r"benchmarks/{0}/{1}/{2}.log".format(heuristic, data_type, filename))
-            
-            while len(results) < len(params[param]):
-                results.append(0)
+            filepath = r"benchmarks/{0}/{1}/{2}.log".format(heuristic, data_type, filename)
+            results = benchmarking.utils.get_r_from_log(filepath)
+            times = benchmarking.utils.get_times_from_log(filepath)
+
+            for lst in [results, times]:
+                while len(lst) < len(params[param]):
+                    lst.append(0)
             results_dict[heuristic] = results
+            times_dict[heuristic] = times
         
-        results_df = pd.DataFrame.from_dict(results_dict)
-        results_df.columns = [param] + column_headers
-        results_df = results_df.round(decimals=4)
+        table_dict = {param_latex[param]: params[param]}
+        for heuristic in heuristics:
+            header = column_headers[heuristic]
+            table_dict["{} $r$".format(header)] = results_dict[heuristic]
+            table_dict["{} $t$".format(header)] = times_dict[heuristic]
+
+        results_df = pd.DataFrame.from_dict(table_dict)
+        results_df = results_df.round(decimals=2)
+
         results_df.to_csv(r"tables/{0}/{1}.csv".format(data_type, filename), index=False, sep="&")
             
