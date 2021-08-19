99fe27dbc8b6e737c9d3070bd28a384d0f156a95
Thomas Holmes <holmesthomas19@gmail.com>
2021-08-19 22:54:43 +0100

    plots

--- src/data_plots.py
+++ src/data_plots.py
@@ -20,21 +20,33 @@ datas = {
     "uniform_ball_with_outliers_3d": data.generation.uniform_ball_with_ouliters(n, 3, 0.9, 1, 2, 3)
 }
 
+
+
 for name, data in datas.items():
     dim = name.split("_")[-1]
+    if name.startswith("hyperspherical_shell"):
+        print(name)
+        c = [np.linalg.norm(x-np.zeros(int(dim[0]))) for x in data]
+        cmap = "winter"
+    else:
+        c = None
+        cmap = None
+
     fig = plt.figure(figsize=(6,6))
     if dim == "2d":
-        plt.scatter(data[:,0], data[:,1], alpha=0.5)
+        plt.scatter(data[:,0], data[:,1], alpha=0.5, c=c, cmap=cmap)
         plt.gca().set_aspect("equal")
     
     if dim == "3d":
+        
         ax = Axes3D(fig, auto_add_to_figure=False)
         fig.add_axes(ax)
 
-        ax.scatter(data[:,0], data[:,1], data[:,2], alpha=0.5)
+        ax.scatter(data[:,0], data[:,1], data[:,2], alpha=0.5, c=c, cmap=cmap)
     
     if dim == "2d":
         plt.savefig(r"images/data_plots/{0}".format(name), bbox_inches='tight')
     else:
         plt.savefig(r"images/data_plots/{0}".format(name), bbox_inches='tight', transparent=True)
+
     plt.clf()
