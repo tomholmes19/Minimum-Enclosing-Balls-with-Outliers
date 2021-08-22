69223e66b21c040a520a0d6da39e6ce9c9309296
Thomas Holmes <holmesthomas19@gmail.com>
2021-08-22 13:54:11 +0100

    example mebwo

--- src/mebwo_example.py
+++ src/mebwo_example.py
@@ -12,9 +12,9 @@ d = 2
 np.random.seed(2000)
 data = data.generation.normal(0,1,n,d)
 M = M_estimate(data)
-ball = MEBwO().fit(data=data, method="relaxation_heuristic", eta=0.9, M=M)
+ball = MEBwO().fit(data=data, method="exact", eta=0.9, M=M, outputflag=1)
 
 ball.plot(data, show=False)
 plt.legend()
-plt.savefig(r"images/sample_mebwo.png")
+plt.savefig(r"images/sample_mebwo.png", bbox_inches="tight")
 plt.show()
